952072747b86365a00cd1c9249ea877f6eb57c89
longieee <long.nguyen-duy@outlook.com>
2023-03-25 14:18:37 +0700

    refactor

--- chap-2/popcount/popcount_test.go
+++ chap-2/popcount/popcount_test.go
@@ -1,19 +1,17 @@
 package popcount
 
 import (
-	"chap-2/popcount"
-	// "fmt"
 	"testing"
 )
 
 func BenchmarkPopCountSingleExpression(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		popcount.PopCount(255)
+		PopCount(255)
 	}
 }
 
 func BenchmarkPopCountLoop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		popcount.PopCountLoop(255)
+		PopCountLoop(255)
 	}
 }
