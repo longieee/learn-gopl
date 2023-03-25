31523cf5e9a6d8ccfb800a9af108d81f2c8eeca1
longieee <long.nguyen-duy@outlook.com>
2023-03-25 14:34:24 +0700

    add extra testing + benchmark

--- chap-2/popcount/popcount_test.go
+++ chap-2/popcount/popcount_test.go
@@ -6,12 +6,58 @@ import (
 
 func BenchmarkPopCountSingleExpression(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		PopCount(255)
+		PopCount(0xFFFFFFFFFFFFFFFF)
 	}
 }
 
 func BenchmarkPopCountLoop(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		PopCountLoop(255)
+		PopCountLoop(0xFFFFFFFFFFFFFFFF)
+	}
+}
+
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0xFFFFFFFFFFFFFFFF)
+	}
+}
+
+func TestPopCountLoop(t *testing.T) {
+	tcs := []struct {
+		number  uint64
+		expects int
+	}{
+		{0x1234567890ABCDEF, 32},
+		{0xFFFFFFFFFFFFFFFF, 64},
+		{0x0000000000000002, 1},
+		{0x0000000000000000, 0},
+		{0x1000000000000000, 1},
+	}
+
+	for _, tc := range tcs {
+		ret := PopCountLoop(tc.number)
+		if ret != tc.expects {
+			t.Errorf("PopCountLoop Failed. Number: %X, expect counts: %d, get: %d", tc.number, tc.expects, ret)
+		}
+	}
+}
+
+func TestPopCountShift(t *testing.T) {
+	tcs := []struct {
+		number  uint64
+		expects int
+	}{
+		{0x1234567890ABCDEF, 32},
+		{0xFFFFFFFFFFFFFFFF, 64},
+		{0x0000000000000002, 1},
+		{0x0000000000000000, 0},
+		{0x1000000000000000, 1},
+	}
+
+	for _, tc := range tcs {
+		ret := PopCountShift(tc.number)
+		if ret != tc.expects {
+			t.Errorf("PopCountShift Failed. Number: %X, expect counts: %d, get: %d", tc.number, tc.expects, ret)
+		}
 	}
 }
