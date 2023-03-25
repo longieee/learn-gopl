c683cb82f4caff3aecc42472936eef6974c0ce19
longieee <long.nguyen-duy@outlook.com>
2023-03-25 14:41:28 +0700

    add ex2.5 Tests & Benchmarks

--- chap-2/popcount/popcount_test.go
+++ chap-2/popcount/popcount_test.go
@@ -4,24 +4,6 @@ import (
 	"testing"
 )
 
-func BenchmarkPopCountSingleExpression(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		PopCount(0xFFFFFFFFFFFFFFFF)
-	}
-}
-
-func BenchmarkPopCountLoop(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		PopCountLoop(0xFFFFFFFFFFFFFFFF)
-	}
-}
-
-func BenchmarkPopCountShift(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		PopCountLoop(0xFFFFFFFFFFFFFFFF)
-	}
-}
-
 func TestPopCountLoop(t *testing.T) {
 	tcs := []struct {
 		number  uint64
@@ -59,5 +41,49 @@ func TestPopCountShift(t *testing.T) {
 		if ret != tc.expects {
 			t.Errorf("PopCountShift Failed. Number: %X, expect counts: %d, get: %d", tc.number, tc.expects, ret)
 		}
+	}
+}
+
+func TestPopCountClearRightMost(t *testing.T) {
+	tcs := []struct {
+		number  uint64
+		expects int
+	}{
+		{0x1234567890ABCDEF, 32},
+		{0xFFFFFFFFFFFFFFFF, 64},
+		{0x0000000000000002, 1},
+		{0x0000000000000000, 0},
+		{0x1000000000000000, 1},
+	}
+
+	for _, tc := range tcs {
+		ret := PopCountClearRightMost(tc.number)
+		if ret != tc.expects {
+			t.Errorf("PopCountClearRightMost Failed. Number: %X, expect counts: %d, get: %d", tc.number, tc.expects, ret)
+		}
+	}
+}
+
+func BenchmarkPopCountSingleExpression(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0xFFFFFFFFFFFFFFFF)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0xFFFFFFFFFFFFFFFF)
+	}
+}
+
+func BenchmarkPopCountShift(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0xFFFFFFFFFFFFFFFF)
+	}
+}
+
+func BenchmarkPopCountClearRightMost(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountClearRightMost(0xFFFFFFFFFFFFFFFF)
 	}
 }
